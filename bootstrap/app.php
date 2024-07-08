86dd81b12713769a5efc65c090119c47b864b06d
Mishan20 <ishansenanayaka1@gmail.com>
2024-07-08 22:55:48 +0530

    edit localization

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->appendToGroup('web', Localization::class);
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'isAdmin' => IsAdmin::class,
