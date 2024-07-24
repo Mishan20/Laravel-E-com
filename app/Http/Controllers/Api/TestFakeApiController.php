10a1e6b7872b000e4c6f09fd7ca2bf8fd8cc5d43
Mishan20 <ishansenanayaka1@gmail.com>
2024-07-25 01:25:40 +0530

    created notifications (Day 21)

--- app/Http/Controllers/Api/TestFakeApiController.php
+++ app/Http/Controllers/Api/TestFakeApiController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Notifications\InvoicePaid;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
 class TestFakeApiController extends Controller
 {
     public function index(){
+
+        Auth::user()->notify(new InvoicePaid());
+        dd("fsfsfs");
+
         $response = Http::get('https://fakestoreapi.com/products');
         $products = $response->json();
 
